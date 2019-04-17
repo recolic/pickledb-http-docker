d8a8db889a55959ba1a9146a58834c4be3b59972
Recolic Keghart <root@recolic.net>
2019-04-16 22:34:25 -0700

    make_private

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ This is a low-performance, slow, unsafe, un-scalable database with only get/set 
 
 However, it's easy to use and easy to deploy for naive usage. For example, use it in your homework!
 
+## Update:
+
+This naive script is ONLY designed for recolic's gitlab project auto-testing report.
+
+See README.md in `https://git.recolic.net/root/cs164_lab2` for usage! (First image uses this tool to fetch latest testing result)
+
--- pickledb_http.py
+++ pickledb_http.py
@@ -12,24 +12,34 @@ db_file = sys.argv[2] if len(sys.argv) > 2 else ''
 db = pickledb.load(db_file, False)
 
 class Handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
         uri = self.path
+        if uri.startswith('/redirect_to_val/'):
+            k_def = uri[17:].split('|')
+            k = k_def[0]
+            default_url = k_def[1] if len(k_def) >= 2 else 'Not_Found'
+            target_url = db.get(k)
+            target_url = default_url if target_url == False else target_url
+            self.send_response(301)
+            self.send_header('Location', target_url)
+            self.send_header('Cache-Control', 'no-cache')
+            self.end_headers()
+            return
+        self.send_response(200)
+        self.send_header('Cache-Control', 'no-cache')
+        self.end_headers()
         message = 'Invalid_Request'
         if uri.startswith('/get/'):
-            kv = uri[5:].split('/')
+            kv = uri[5:].split('|')
             if len(kv) == 1:
                 message = db.get(kv[0])
                 if message == False:
                     message = ''
         elif uri.startswith('/set/'):
-            kv = uri[5:].split('/')
+            kv = uri[5:].split('|')
             if len(kv) == 2:
                 message = str(db.set(kv[0], kv[1]))
         self.wfile.write(message.encode('utf-8'))
-        #self.wfile.write('\n')
         return
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
